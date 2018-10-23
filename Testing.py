c2f3ff597c9d18d4f923fec9e65d41623d3dbeb8
adam2809 <adam.kule@gmail.com>
2018-10-23 12:04:01 +0100

    Roughly finished scraper, results still need formatting

--- Testing.py
+++ Testing.py
@@ -2,48 +2,57 @@ import unittest
 from Wiki_Info import scraper
 
 class ScraperTests(unittest.TestCase):
-    def testUniversitites(self):
+    def testNottingham(self):
         print('Testing University of Nottingham')
         uniinfo = scraper('University of Nottingham')
-        self.assertEquals(uniinfo['Motto'],'Sapientia urbs conditur')
-        self.assertEquals(uniinfo['Chancellor'],'Sir Andrew Witty')
-        self.assertEquals(uniinfo['Budget'],'£637.6 million (2016-17)[1]')
-        self.assertEquals(uniinfo['Location'],'Nottingham, England, UK')
+        self.assertEqual(uniinfo['Motto'],'Sapientia urbs conditur (Latin)')
+        self.assertEqual(uniinfo['Chancellor'],'Sir Andrew Witty[2]')
+        self.assertEqual(uniinfo['Budget'],'£637.6 million (2016-17)[1]')
+        self.assertEqual(uniinfo['Location'],'Nottingham, England, UK')
         print()
 
+
+    def testWarsaw(self):
         print('Testing University of Warsaw')
         uniinfo = scraper('University of Warsaw')
-        self.assertEquals(uniinfo['Type'],'Public')
-        self.assertEquals(uniinfo['Established'],'1816 (202 years ago)')
-        self.assertEquals(uniinfo['Location'],'Sapientia urbs conditur')
+        self.assertEqual(uniinfo['Type'],'Public')
+        self.assertEqual(uniinfo['Established'],'1816  (202\xa0years ago)')
+        self.assertEqual(uniinfo['Location'],'Warsaw, Poland')
         print()
 
+
+    def testSantoTomas(self):
         print('Testing University of Santo Tomas')
         uniinfo = scraper('University of Santo Tomas')
-        self.assertEquals(uniinfo['Motto'],'Veritas in Caritate')
-        self.assertEquals(uniinfo['Chancellor'],'Very Rev. Fr. Bruno Cadoré, O.P., M.D.')
-        self.assertEquals(uniinfo['Location'],'España Boulevard, Sampaloc, Manila, Philippines')
+        self.assertEqual(uniinfo['Motto'],'Veritas in Caritate')
+        self.assertEqual(uniinfo['Chancellor'],'Very Rev. Fr. Bruno Cadoré, O.P., M.D.')
+        self.assertEqual(uniinfo['Location'],'España Boulevard, Sampaloc, Manila, Philippines')
         print()
 
+
+    def testPeking(self):
         print('Testing Peking University')
         uniinfo = scraper('Peking University')
-        self.assertEquals(uniinfo['Chancellor'],'Very Rev. Fr. Bruno Cadoré, O.P., M.D.')
-        self.assertEquals(uniinfo['President'],'Lin Jianhua')
-        self.assertEquals(uniinfo['Location'],'Haidian District, Beijing, China')
+        self.assertEqual(uniinfo['President'],'Hao Ping')
+        self.assertEqual(uniinfo['Location'],'Haidian District, Beijing, China')
         print()
 
+
+    def testChihuahua(self):
         print('Testing Chihuahua Institute of Technology')
         uniinfo = scraper('Chihuahua Institute of Technology')
-        self.assertEquals(uniinfo['Motto'],'La Técnica por el engrandecimiento de México')
-        self.assertEquals(uniinfo['Motto in English'],'Technology for the aggrandizement of Mexico')
-        self.assertEquals(uniinfo['Established'],'1948')
+        self.assertEqual(uniinfo['Motto'],'La Técnica por el engrandecimiento de México')
+        self.assertEqual(uniinfo['Motto in\xa0English'],'Technology for the aggrandizement of Mexico')
+        self.assertEqual(uniinfo['Established'],'1948')
         print()
 
+
+    def testPardubice(self):
         print('Testing University of Pardubice')
         uniinfo = scraper('University of Pardubice')
-        self.assertEquals(uniinfo['Established'],'1950')
-        self.assertEquals(uniinfo['Students'],'9,200')
-        self.assertEquals(uniinfo['Location'],'Pardubice, Czech Republic')
+        self.assertEqual(uniinfo['Established'],'1950')
+        self.assertEqual(uniinfo['Students'],'9,200')
+        self.assertEqual(uniinfo['Location'],'Pardubice, Czech Republic')
         print()
 
 
--- Wiki_Info.py
+++ Wiki_Info.py
@@ -1,5 +1,6 @@
 import requests, json
 from lxml import etree
+from bs4 import BeautifulSoup
 
 def scraper(college):
     #Tested with University of Toronto, Chan Sui Ki, University of Santo Tomas, Far Eastern University,
@@ -20,48 +21,11 @@ def scraper(college):
     #Make the request for the page requested by the user
     result_url = 'https://en.wikipedia.org/wiki?curid=%d' % page_id
 
-    result_req = requests.get(result_url)
-
-    store = etree.fromstring(result_req.text)
-
-    #Look for the college motto
-    output_motto = store.xpath('//table[@class="infobox vcard"]/tbody/tr[th/text()="Motto"]/td/i')
-
-    if len(output_motto) >= 1:
-        print('Motto: ' + output_motto[0].text)
-
-    else:
-        print('[!]No motto here')
-
-
-    # Looking for College Chancellor name
-    output_chan = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[8]/td/a')
-
-    if(len(output_chan)>= 1):
-        print("Chancellor: "+output_chan[0].text)
-
-    else:
-        print('[!]No Chancellor found')
-
-    #Looking for college President
-    output_pres = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[9]/td/a')
-    if(len(output_pres)>=1):
-        print("President: "+output_pres[0].text)
-    else:
-        print('[!]No President found')
-
-    #Looking for College Location
-    output_loc = store.xpath('//*[@id="mw-content-text"]/div/table[1]/tbody/tr[15]/td/span[1]/a')
-    if(len(output_loc)>= 1):
-        print("Location: "+output_loc[0].text)
-    else:
-        print('[!]No Location Found')
-
-    #Look for the college motto
-    output = store.xpath('//table[@class="infobox vcard"]/tbody/tr[th/text()="Motto"]/td/i')
-    dict_items= {"Motto": output_motto[0].text,
-                 "Chancellor": output_chan[0].text,
-                 "President": output_pres[0].text,
-                 "Location": output_loc[0].text}
-
-    return dict_items
+    result_req = requests.get(result_url).text
+    soup = BeautifulSoup(result_req,features='lxml')
+    table = soup.find('table',class_='infobox vcard')
+    result = {}
+    for row in table.find_all('tr'):
+        if row.find('th'):
+            result[row.find('th').text] = row.find('td').text
+    return {key.strip():result[key].strip() for key in result}
